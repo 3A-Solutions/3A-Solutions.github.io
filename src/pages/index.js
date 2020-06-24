ec4455d0639c17598f066a2ff2dcce7fd438cff9
Adam Varga <adam@vrg.hu>
2020-06-24 22:52:39 +0200

    Basic setup with deployment

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const IndexPage = () => <><h1>WELCOME TO 3A!!</h1>
+ <Link to="/page-2/">Go to page 2</Link> <br />
+</>
+
+export default IndexPage
---
+++ src/pages/page-2.js
@@ -0,0 +1,8 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const SecondPage = () => <><h1>WELCOME TO 3A's second page!!!!</h1>
+ <Link to="/">Go Home</Link> <br />
+</>
+
+export default SecondPage
