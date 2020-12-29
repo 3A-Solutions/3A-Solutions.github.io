9778b556e0bad6754beeedbc1b85e5c5d35bb174
Adam Varga <adam@vrg.hu>
2020-12-29 10:17:36 +0100

    Updates

---
+++ component---src-pages-index-js-dee4d77cff9417d47bde.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{RXBc:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),l=t.n(a),r=t("Wbzz");n.default=function(){return l.a.createElement(l.a.Fragment,null,l.a.createElement("h1",null,"WELCOME TO 3A!!"),l.a.createElement(r.Link,{to:"/page-2/"},"Go to page 2")," ",l.a.createElement("br",null))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-dee4d77cff9417d47bde.js.map
